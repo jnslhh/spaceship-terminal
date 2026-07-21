2146da50bd24c5177f233a90c32be25f166a4b60
jnslhh <gysik1234@gmail.com>
2026-07-21 21:15:44 +0500

    Adding vertical movement and fire animation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import time
 
 # spaceship
-RAW_BRAILLE_ART = r"""
+FRAME_0 = r"""
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠶
 ⠀⠀⠀⠀⠀⠀⠈⠛⠛⢃⣀⣀⠀⠀⠀⠉⠰⢶⣶⣶⣶⣶⠶⠶
 ⠀⠀⠀⠀⠉⠀⠀⠀⠀⠈⠉⠉⠰⠶⠆⣤⠀⠀⢸⣿⣿⣷⣽⣢⣄
@@ -20,8 +20,75 @@ RAW_BRAILLE_ART = r"""
 ⠀⠀⠀⠀⠉⠀⢀⣀⣀⡈⠉⠉⠀⠀⠀⠤⠠⠼⠿⠿⠯⠷⠭⠤
 ⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠤⠀
 """
+FRAME_1 = r"""
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠂
+⠀⠀⠀⠀⠀⠀⠀⠘⠓⢄⣀⡀⠀⠀⠀⠉⠰⢶⣶⣶⣶⣶⠶⠶
+⠀⠀⠀⠀⠡⠀⠀⠀⠀⠀⠉⠉⠰⠤⠂⣤⠀⠀⢸⣿⣿⣷⣽⣢⣄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⢹⣿⣿⣿⣷⣯⣖⣤⣀⣀⣀⣀⣀⣀⡀
+⠀⠀⠀⠀⠒⠐⠒⠒⠐⠒⠒⠐⠒⠒⠰⢶⣼⣿⣿⡟⡏⢿⣿⣿⣿⢻⣧⣶⣴⣶⣴⣶⣴⣦⠉⠱⣦⣤⣤⣤
+⠀⠀⠀⠀⠀⠤⠀⠤⠤⠀⠤⠤⠤⠐⠚⠈⠙⢻⣿⣷⣧⣿⣿⣿⣿⣾⣧⣿⣿⣿⣿⣿⣿⣿⠶⠞⠋
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣿⣻⣟⣛⣻⣿⣿⣷⣾⣷⣶⣾⣽⢖⣶⣭⠭⣛⢖⣒⡦⠤⣀⡀
+⠀⠀⠀⠡⠈⢁⠈⢉⢁⣈⠉⢉⢁⠉⢉⢸⣿⣿⣿⣿⡇⣿⣿⣟⡯⣻⣿⣿⣾⣿⣿⢹⣿⣟⠉⠀⠹⡏⢿⣿⣿⠿⣟⠆
+⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠉⠉⠈⠉⠀⠈⠙⠛⣿⣵⣯⣭⣽⣿⣿⣭⣿⣯⣭⣽⣿⣮⣿⣿⣶⣯⣮⣭⠷⠶⠉⠁
+⠀⠀⠀⠀⢠⣀⢠⣀⣀⠀⣀⣀⣀⢠⣀⢀⣠⣴⣶⣯⡝⣯⣭⣭⣭⣽⡗⣚⣓⣒⣒⣛⣒⣒⠭⢍⠉
+⠀⠀⠀⠡⠀⠠⠠⠀⠠⠠⠠⠀⠠⠠⠠⠼⢿⣿⣿⣧⡇⣿⣿⣿⣿⣼⡓⠿⠻⣿⠻⠿⢻⡟⣀⣠⠿⠒⠒⠒⠂
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠀⢹⣷⣶⣶⣶⡾⣿⠟⠛⠛⠛⠛⠛⠛⠉⠁
+⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⢀⠠⠤⠔⠛⠀⠀⢰⣿⣿⣿⣻⡕⠋
+⠀⠀⠀⠀⠀⠀⠀⣀⣀⡈⠉⠉⠀⠀⠀⠤⠠⠼⠿⠿⠯⠷⠭⠤
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠤⠀
+"""
 
-SHIP_ART = [line for line in RAW_BRAILLE_ART.splitlines() if line]
+FRAME_2 = r"""
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⠉⠰⢶⣶⣶⣶⣶⠶⠶
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠤⠆⣤⠀⠀⢸⣿⣿⣷⣽⣢⣄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⢹⣿⣿⣿⣷⣯⣖⣤⣀⣀⣀⣀⣀⣀⡀
+⠀⠀⠀⠀⠀⠀⠤⠤⠤⠤⠤⠤⠤⠤⠤⠰⢶⣼⣿⣿⡟⡏⢿⣿⣿⣿⢻⣧⣶⣴⣶⣴⣶⣴⣦⠉⠱⣦⣤⣤⣤
+⠀⠀⠀⠀⠀⠀⠀⠐⠒⠐⠒⠒⠒⠐⠚⠈⠙⢻⣿⣷⣧⣿⣿⣿⣿⣾⣧⣿⣿⣿⣿⣿⣿⣿⠶⠞⠋
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣿⣻⣟⣛⣻⣿⣿⣷⣾⣷⣶⣾⣽⢖⣶⣭⠭⣛⢖⣒⡦⠤⣀⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⣈⠉⣉⣁⠉⣉⢸⣿⣿⣿⣿⡇⣿⣿⣟⡯⣻⣿⣿⣾⣿⣿⢹⣿⣟⠉⠀⠹⡏⢿⣿⣿⠿⣟⠆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠈⠉⠀⠈⠙⠛⣿⣵⣯⣭⣽⣿⣿⣭⣿⣯⣭⣽⣿⣮⣿⣿⣶⣯⣮⣭⠷⠶⠉⠁
+⠀⠀⠀⠀⠀⠀⢀⣤⣤⠀⣤⣤⣤⢠⣤⢀⣠⣴⣶⣯⡝⣯⣭⣭⣭⣽⡗⣚⣓⣒⣒⣛⣒⣒⠭⢍⠉
+⠀⠀⠀⠀⠀⠀⠠⠤⠤⠄⠤⠤⠄⠤⠤⠠⠼⢿⣿⣿⣧⡇⣿⣿⣿⣿⣼⡓⠿⠻⣿⠻⠿⢻⡟⣀⣠⠿⠒⠒⠒⠂
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠀⢹⣷⣶⣶⣶⡾⣿⠟⠛⠛⠛⠛⠛⠛⠉⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠤⠔⠛⠀⠀⢰⣿⣿⣿⣻⡕⠋
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡈⠉⠉⠀⠀⠀⠤⠠⠼⠿⠿⠯⠷⠭⠤
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠀
+"""
+
+FRAME_3 = r"""
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠢
+⠀⠀⠀⠀⠀⠐⠈⠛⢃⢀⡀⠀⠀⠀⠉⠰⢶⣶⣶⣶⣶⠶⠶
+⠀⠀⠀⠀⠂⠀⠀⠀⠀⠀⠀⠉⠰⠢⠔⣤⠀⠀⢸⣿⣿⣷⣽⣢⣄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⢹⣿⣿⣿⣷⣯⣖⣤⣀⣀⣀⣀⣀⣀⡀
+⠀⠀⠤⠢⠤⠤⠤⠠⠤⠤⠢⠤⠤⠠⢶⣼⣿⣿⡟⡏⢿⣿⣿⣿⢻⣧⣶⣴⣶⣴⣶⣴⣦⠉⠱⣦⣤⣤⣤
+⠀⠀⠀⠡⠐⠡⠐⠡⠀⠡⠐⠂⠐⠚⠈⠙⢻⣿⣷⣧⣿⣿⣿⣿⣾⣧⣿⣿⣿⣿⣿⣿⣿⠶⠞⠋
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣿⣻⣟⣛⣻⣿⣿⣷⣾⣷⣶⣾⣽⢖⣶⣭⠭⣛⢖⣒⡦⠤⣀⡀
+⠀⠀⠡⠈⢄⠈⢄⡈⠉⢄⡈⠉⢄⢸⣿⣿⣿⣿⡇⣿⣿⣟⡯⣻⣿⣿⣾⣿⣿⢹⣿⣟⠉⠀⠹⡏⢿⣿⣿⠿⣟⠆
+⠀⠀⠀⠀⠁⠀⠈⠉⠁⠀⠉⠁⠈⠁⠀⠈⠙⠛⣿⣵⣯⣭⣽⣿⣿⣭⣿⣯⣭⣽⣿⣮⣿⣿⣶⣯⣮⣭⠷⠶⠉⠁
+⠀⠀⠀⢀⣠⢀⣠⣄⠀⣠⣄⣄⢀⣄⢀⣠⣴⣶⣯⡝⣯⣭⣭⣭⣽⡗⣚⣓⣒⣒⣛⣒⣒⠭⢍⠉
+⠀⠀⠡⢀⠡⠡⢀⠠⠡⠡⢀⠡⠡⢀⠼⢿⣿⣿⣧⡇⣿⣿⣿⣿⣼⡓⠿⠻⣿⠻⠿⢻⡟⣀⣠⠿⠒⠒⠒⠂
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠀⢹⣷⣶⣶⣶⡾⣿⠟⠛⠛⠛⠛⠛⠛⠉⠁
+⠀⠀⠀⠀⢀⠀⠀⠀⠀⢀⢀⠀⠠⠤⠔⠛⠀⠀⢰⣿⣿⣿⣻⡕⠋
+⠀⠀⠀⠀⠁⠀⢀⢀⢀⡈⠉⠁⠀⠀⠀⠤⠠⠼⠿⠿⠯⠷⠭⠤
+⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁⠤⠀
+"""
+SHIP_FRAMES = [
+    [line for line in FRAME_0.splitlines() if line],
+    [line for line in FRAME_1.splitlines() if line],
+    [line for line in FRAME_2.splitlines() if line],
+    [line for line in FRAME_3.splitlines() if line]
+]
+
+for frame in SHIP_FRAMES:
+    for i in range(len(frame)):
+        target_len = len(SHIP_FRAMES[0][i])
+        current_len = len(frame[i])
+        
+        if current_len < target_len:
+            frame[i] = " " * (target_len - current_len) + frame[i]
+        elif current_len > target_len:
+            frame[i] = frame[i][current_len - target_len:]
 
 # Dynamic change in the number of stars for the current screen area
 def adjust_stars(stars, max_y, max_x):
@@ -54,10 +121,13 @@ def main(stdscr):
     adjust_stars(stars, max_y, max_x)
         
     # spaceship position
-    ship_y = max_y // 2 - (len(SHIP_ART) // 2)
-    ship_x = 5
+    ship_y = max_y // 2 - (len(SHIP_FRAMES[0]) // 2)
+    ship_x = 2
     
     frame_count = 0
+
+    shipDirection = 1 # -1 - up, 1 - down
+    shipSpeed = 30
 
     while True:
         key = stdscr.getch()
@@ -70,9 +140,22 @@ def main(stdscr):
 
             max_y, max_x = stdscr.getmaxyx() # update screen size
 
-            ship_y = max_y // 2 - (len(SHIP_ART) // 2) # update the spaceship position
+            ship_y = max_y // 2 - (len(SHIP_FRAMES[0]) // 2) # update the spaceship position
 
             adjust_stars(stars, max_y, max_x)
+
+        if frame_count % shipSpeed == 0:
+            ship_y += shipDirection
+
+            # check upper limit
+            if ship_y <= 1:
+                ship_y = 1
+                shipDirection = 1
+            
+            # check lower limit
+            elif ship_y >= max_y - len(SHIP_FRAMES[0]) - 1:
+                ship_y = max_y - len(SHIP_FRAMES[0]) - 1
+                shipDirection = -1
 
         # 1. update star coordinates
         frame_count += 1
@@ -98,9 +181,13 @@ def main(stdscr):
                 pass 
 
         # 4. drawing spaceship
-        for i, line in enumerate(SHIP_ART):
+        current_frame_index = (frame_count // 4) % 4
+        
+        current_ship_art = SHIP_FRAMES[current_frame_index]
+
+        for i, line in enumerate(current_ship_art):
             try:
-                stdscr.addstr(ship_y + i, ship_x, line)
+                stdscr.addstr(ship_y + i, ship_x, line)    
             except curses.error:
                 pass
 
